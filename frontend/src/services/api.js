49c1de6067abcd81d5ba3db38e3c9127b0595236
bagasdprs <bagasdprs@gmail.com>
2026-01-21 11:16:27 +0700

    Update API Config for Deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: BASE_URL,
+  withCredentials: true,
 });
 
 API.interceptors.request.use((req) => {
-  if (localStorage.getItem("token")) {
-    req.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  if (localStorage.getItem("user")) {
+    const user = JSON.parse(localStorage.getItem("user"));
+    req.headers.Authorization = `Bearer ${user.token}`;
   }
   return req;
 });
