e8e0655679fb701c0eef4133e44d2dae245a8225
bagasdprs <bagasdprs@gmail.com>
2026-01-21 11:57:48 +0700

    FIX: update CORS spesific origin

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://talent-growth-blog-platform-fronten.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
