874871670c016e8e420b1f6f3c091b1ca7876020
bagasdprs <bagasdprs@gmail.com>
2026-01-21 12:02:01 +0700

    Fix: Update CORS origin

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "https://talent-growth-blog-platform-fronten.vercel.app",
+    origin: "https://blog-tg-app.vercel.app",
     credentials: true,
   })
 );
